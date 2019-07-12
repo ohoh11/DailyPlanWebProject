ddf5f6fcfbc2816198ebd7b284a9feb07f4f794b
ohoh11 <www.1505435988@qq.com>
2019-07-12 10:34:35 +0800

    添加项目文件。

---
+++ DailyPlanWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DailyPlanWebApp.Startup))]
+namespace DailyPlanWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
